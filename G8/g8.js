a3888c1d7846bc0ab1d3c18dd150153027bb0ea5
Anna Sophia Wolniewicz <annasophia@wolniewicz.com>
2018-01-05 13:28:34 -0700

    Started my second bot, G8. Works and does the basic ping --> pong! command! .

---
+++ G8/g8.js
@@ -0,0 +1,25 @@
+const Discord = require("discord.js");
+const client = new Discord.Client();
+
+client.on("ready", () => {
+  console.log('I am ready!');
+});
+
+client.on("message", (message) => {
+  if (message.content.startsWith("ping")) {
+    message.channel.send("Pong!");
+  }
+  // If the message is "what is my avatar"
+ if (message.content === 'what is my avatar') {
+   // Send the user's avatar URL
+   message.reply(message.author.avatarURL);
+ }
+
+ // check to see if message is a bad word
+ if (message.content === 'poop') {
+   // send emssage and alert them
+   message.reply(message.author, 'bad word');
+ }
+});
+
+client.login("Mzk4OTI0ODc3NjYwMjkxMDcz.DTFo1g.XeaF8Z3Ct9x0hbdH2yyTO3rINvg");
--- README.md
+++ README.md
@@ -4,3 +4,7 @@
 ### McBot
 * A simple bot that will respond to basic commands, does not know how to moderate.
 * https://medium.com/@renesansz/tutorial-creating-a-simple-discord-bot-9465a2764dc0
+
+### G8
+* The bot I am currently working on. I am following a tutorial on how to work with discord.js.
+* https://anidiots.guide/getting-started/the-long-version.html 
